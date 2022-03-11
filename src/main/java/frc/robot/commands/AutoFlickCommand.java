647b6f4157a5a4965d2f93af8d1bee05693a663f
GiulioA <giulio.alessi@acecharterhigh.org>
2022-03-10 17:52:30 -0800

    Added Auto Flick Command

---
+++ src/main/java/frc/robot/commands/AutoFlickCommand.java
@@ -0,0 +1,43 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.subsystems.LauncherSubsystem;
+
+public class AutoFlickCommand extends Command {
+	private LauncherSubsystem _launcher;
+
+	public AutoFlickCommand() {
+		_launcher = LauncherSubsystem.getInstance();
+	}
+	@Override
+	protected void initialize()
+	{
+
+	}
+
+	@Override
+	protected void execute()
+	{
+		_launcher.bothMotorsOn();
+		_launcher.solenoidOn();
+	}
+
+	@Override
+	protected boolean isFinished()
+	{
+		return false;
+	}
+
+	@Override
+	protected void interrupted()
+	{
+		end();
+	}
+
+	@Override
+	protected void end()
+	{
+		_launcher.bothMotorsOff();
+		_launcher.solenoidOff();
+	}
+}
